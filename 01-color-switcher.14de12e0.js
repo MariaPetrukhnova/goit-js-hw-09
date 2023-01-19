16e20dbcd8cf81dfb3218e36e242dd77a8ba8068
MariaPetrukhnova <110037119+MariaPetrukhnova@users.noreply.github.com>
2023-01-19 08:48:19 +0000

    Deploying to gh-pages from @ MariaPetrukhnova/goit-js-hw-09@c5516ae52649c02b57d37f363a031404bcc7f940 🚀

--- 01-color-switcher.14de12e0.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.body,o=null;t.addEventListener("click",(function(){o=setInterval((function(){var e="#".concat(Math.floor(16777215*Math.random()).toString(16));n.style.backgroundColor=e,t.setAttribute("disabled","disabled")}),1e3)})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled","disabled")}))}();
-//# sourceMappingURL=01-color-switcher.14de12e0.js.map
---
+++ 01-color-switcher.4a2d0a9a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.body;let o=null;t.addEventListener("click",(function(){t.setAttribute("disabled","disabled"),o=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;n.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled","disabled")}));
+//# sourceMappingURL=01-color-switcher.4a2d0a9a.js.map
